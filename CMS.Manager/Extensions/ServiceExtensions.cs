1c5c5e52eeef243e56d5db4e98023797a0e90b2a
an.tran <an.tran@ANTRAN>
2024-02-03 15:51:30 +0700

    Add project files.

---
+++ CMS.Manager/Extensions/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using CMS.Contracts.Logger;
+using CMS.LoggerService;
+
+namespace CMS.Manager.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+            builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+        });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+        services.Configure<IISOptions>(options =>
+        {
+        });
+
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+        services.AddSingleton<ILoggerManager, LoggerManager>();
+    }
+}
